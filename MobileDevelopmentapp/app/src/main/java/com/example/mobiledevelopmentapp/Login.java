bf1ed0c8458f9e50773850778224660509ee0de6
高杰 <14369579+gaojiedot@user.noreply.gitee.com>
2024-06-08 23:05:52 +0800

    1

---
+++ MobileDevelopmentapp/app/src/main/java/com/example/mobiledevelopmentapp/Login.java
@@ -0,0 +1,33 @@
+package com.example.mobiledevelopmentapp;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class Login extends AppCompatActivity {
+
+
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+         button = findViewById(R.id.login_button);
+    }
+
+    public void onclick(View view) {
+       ;
+    }
+}
