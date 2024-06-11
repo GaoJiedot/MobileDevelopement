80b2d7fbaa20f9225cbcf9b4d28ddd2779ce20e4
高杰 <14369579+gaojiedot@user.noreply.gitee.com>
2024-06-11 22:54:38 +0800

    找回版本

--- DevelopementApp/app/src/main/java/com/example/fragment/ActionLibraryFragment.java
+++ DevelopementApp/app/src/main/java/com/example/fragment/ActionLibraryFragment.java
@@ -13,6 +13,7 @@ public class ActionLibraryFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.actionlibrary, container, false);
 
     }
--- DevelopementApp/app/src/main/java/com/example/fragment/MessageFragment.java
+++ DevelopementApp/app/src/main/java/com/example/fragment/MessageFragment.java
@@ -1,34 +1,19 @@
 package com.example.fragment;
 
-import android.app.Dialog;
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.Window;
-import android.widget.Button;
-import android.widget.ImageView;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.example.myapplication.R;
 
 public class MessageFragment extends Fragment {
-
-    private static final int PICK_IMAGE_REQUEST = 1;
-    private ImageView msg_Add_iv;
-    private ImageView imageView;
-
-    @Nullable
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.message, container, false);
     }
-
 }
