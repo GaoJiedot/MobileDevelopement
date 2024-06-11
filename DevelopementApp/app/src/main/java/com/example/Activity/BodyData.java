260135965dc2ff2ac3ba14005717b43705e9ae1c
高杰 <14369579+gaojiedot@user.noreply.gitee.com>
2024-06-11 13:19:32 +0800

    完成已有界面的跳转

--- DevelopementApp/app/src/main/java/com/example/Activity/BodyData.java
+++ DevelopementApp/app/src/main/java/com/example/Activity/BodyData.java
@@ -1,6 +1,9 @@
 package com.example.Activity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,9 +11,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.myapplication.R;
 
 public class BodyData extends AppCompatActivity {
+    private ImageView bd_Back_iv;
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.bodydata);
+        bd_Back_iv=(ImageView) findViewById(R.id.bd_back_iv);
+        bd_Back_iv.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(BodyData.this,Home.class));
+                finish();
+            }
+        });
     }
 }
