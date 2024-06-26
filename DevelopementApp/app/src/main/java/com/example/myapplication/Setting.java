ffc9570aa8ba6eb7533081647fd8f834c2020251
高杰 <14369579+gaojiedot@user.noreply.gitee.com>
2024-06-26 09:46:36 +0800

    完成数据绑定和跳转

--- DevelopementApp/app/src/main/java/com/example/myapplication/Setting.java
+++ DevelopementApp/app/src/main/java/com/example/myapplication/Setting.java
@@ -1,17 +1,26 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-public class Setting extends AppCompatActivity {
 
+public class Setting extends AppCompatActivity {
+    private ImageView imageView;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting_in);
-
+        imageView = findViewById(R.id.set_back);
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
 
     }
 }
